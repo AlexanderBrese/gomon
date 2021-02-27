285c54b558226207b5c6cfdd347ab56f615880a9
Alexander Brese <brese@smark.de>
2021-02-27 01:09:17 +0100

    configuration model

--- configuration.go
+++ configuration.go
@@ -1,41 +1,46 @@
 package main
 
 import (
-	"path/filepath"
 	"time"
 )
 
-const DFT = "go-server-browser-reload.toml"
-const ROOT_PATH = "."
-
+// Configuration is a in-memory representation of the expected configuration file
 type Configuration struct {
 	SourceDir   string   `toml:"relative_source_dir"`
 	BuildDir    string   `toml:"relative_build_dir"`
 	LogDir      string   `toml:"relative_log_dir"`
-	WatchExt    []string `toml:"watch_ext"`
-	IgnoreDir   []string `toml:"ignore_dir"`
-	WatchDir    []string `toml:"include_dir"`
-	IgnoreFiles []string `toml:"ignore_files"`
+	WatchExt    []string `toml:"watch_relative_ext"`
+	IgnoreDir   []string `toml:"ignore_relative_dir"`
+	WatchDir    []string `toml:"watch_relative_dir"`
+	IgnoreFiles []string `toml:"ignore_relative_files"`
 	Delay       int      `toml:"delay"`
 	Port        int      `toml:"port"`
+}
+
+var defaultConfiguration = &Configuration{
+	SourceDir:   "cmd/web",
+	BuildDir:    "tmp/build",
+	LogDir:      "tmp/go-server-browser-reload.log",
+	WatchExt:    []string{"go", "tpl", "tmpl", "html", "css", "js", "env", "yaml"},
+	IgnoreDir:   []string{"assets", "tmp", "vendor", "node_modules", "build"},
+	WatchDir:    []string{},
+	IgnoreFiles: []string{},
+	Delay:       1000,
+	Port:        3000,
 }
 
 func (c *Configuration) delay() time.Duration {
 	return time.Duration(c.Delay) * time.Millisecond
 }
 
-func (c *Configuration) srcPath() string {
-	return _rel(c.SourceDir)
+func (c *Configuration) srcPath() (string, error) {
+	return absolutePath(c.SourceDir)
 }
 
-func (c *Configuration) buildPath() string {
-	return _rel(c.BuildDir)
+func (c *Configuration) buildPath() (string, error) {
+	return absolutePath(c.BuildDir)
 }
 
-func (c *Configuration) logPath() string {
-	return _rel(c.LogDir)
-}
-
-func _rel(path string) string {
-	return filepath.Join(ROOT_PATH, path)
+func (c *Configuration) logPath() (string, error) {
+	return absolutePath(c.LogDir)
 }
