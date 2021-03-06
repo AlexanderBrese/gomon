70855c5ed79afc90feeea05f1ab1c8a250cf04d3
Alexander Brese <brese@smark.de>
2021-03-06 09:24:08 +0100

    implement: windows cmd tool

---
+++ pkg/reload/cmd.go
@@ -0,0 +1,38 @@
+package reload
+
+import (
+	"io"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+func (r *Reload) killCmd(cmd *exec.Cmd) (pid int, err error) {
+	pid = cmd.Process.Pid
+	// https://stackoverflow.com/a/44551450
+	kill := exec.Command("TASKKILL", "/T", "/F", "/PID", strconv.Itoa(pid))
+	return pid, kill.Run()
+}
+
+func (r *Reload) startCmd(cmd string) (*exec.Cmd, io.ReadCloser, io.ReadCloser, error) {
+	var err error
+
+	if !strings.Contains(cmd, ".exe") {
+		//e.runnerLog("CMD will not recognize non .exe file for execution, path: %s", cmd)
+	}
+
+	c := exec.Command("cmd", "/c", cmd)
+	stderr, err := c.StderrPipe()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	stdout, err := c.StdoutPipe()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	err = c.Start()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	return c, stdout, stderr, err
+}
